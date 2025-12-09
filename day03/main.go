4d6e531bffaf257fdd54adf192f21897b3f291b9
Casey Capetola <caseycapetola@gmail.com>
2025-12-09 13:42:17 -0500

    Initial iterative implementation for day 3

--- day03/main.go
+++ day03/main.go
@@ -75,9 +75,82 @@ func part2(inputFile string, numBatteries int) {
 	fmt.Printf("Part2: %d\n", res)
 }
 
+type Battery struct {
+	index int
+	value int
+}
+
+// func iterativePart2(inputFile string, numBatteries int) {
+// 	file, _ := os.Open(inputFile)
+// 	defer file.Close()
+
+// 	res := 0
+
+// 	scanner := bufio.NewScanner(file)
+// 	for scanner.Scan() {
+// 		line := scanner.Text()
+
+// 		l, r := 0, 0
+
+// 		batteries := []byte{}
+
+// 		for r < len(line) {
+// 			temp := min(numBatteries, len(line)-r)
+// 			fmt.Printf("Batteries: %s; value to compare: %d; temp: %d\n", string(batteries[:]), byteToInt(line[r]), temp)
+// 			if len(batteries) + (len(line)-r) == numBatteries { // Append the remaining content
+// 				fmt.Println("APPENDING REMAINING CONTENT")
+// 				for r < len(line) {
+// 					batteries = append(batteries, line[r])
+// 					r += 1
+// 				}
+// 			} else if len(batteries) > 0 && byteToInt(batteries[numBatteries-temp]) < byteToInt(line[r]) {
+// 				batteries = append(batteries[:numBatteries-temp], line[r])
+// 			} else if len(batteries) < numBatteries {
+// 				fmt.Println("IN DEFAULT")
+// 				l = r
+// 				for len(line)-r > temp {
+// 					if byteToInt(line[r]) > byteToInt(line[l]) {
+// 						l = r
+// 						break
+// 					}
+// 					r += 1
+// 				}
+				
+// 				batteries = append(batteries, line[l])
+// 			}
+// 			r+=1
+// 		}
+
+// 		// Let l = the index of the start of the non-increasing subsequence
+// 		for r < len(line) {
+// 			temp := min(numBatteries, len(line)-r)
+
+// 			if len(batteries) > 0 && byteToInt(batteries[numBatteries-temp]) < byteToInt(line[r]) { // Handles remainder stuff
+// 				batteries = append(batteries[:numBatteries-temp], line[r])
+// 			} else if len(batteries) == 0 { // Need to add an initial value to the slice
+// 				batteries = append(batteries, line[r])
+// 			} else if byteToInt(line[r]) > byteToInt(line[r-1]){
+// 				// We will walk through our array to find the optimal place to put this value
+// 				for byteToInt(line[l]) >= byteToInt(line[r]) &&  {
+// 					l += 1
+// 				}
+// 			}
+
+// 			r += 1
+// 		}
+
+// 		joltage, _ := strconv.Atoi(string(batteries[:]))
+// 		fmt.Printf("Joltage: %d\n", joltage)
+// 		res += joltage
+		
+// 	}
+// 	fmt.Printf("Part2: %d\n", res)
+// }
+
 func main() {
 	part1("sample_input.txt")
 	part1("input.txt")
 	part2("sample_input.txt", 12)
 	part2("input.txt", 12)
+	// iterativePart2("one_line.txt", 12)
 }
